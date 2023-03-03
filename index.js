be2ba7a34e924fa66498f473247e14db83a1305f
Jarvis Brown <brownjarvis21@gmail.com>
2023-03-03 00:55:30 -0500

    pass

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution in this file!
+const employee = {
+  name: "Jarvis",
+  streetAddress: "35 Saint Louis",
+};
+
+const person = {
+  name: "Amber"
+}
+
+const Amber = "person"
+
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+  const newEmployee = {...object};
+  newEmployee[key] = value;
+
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+  employee.name = "Sam"
+  employee.streetAddress = "12 Broadway"
+
+  return employee;
+}
+
+function deleteFromEmployeeByKey(object, key, value) {
+  const newEmployee = {...object};
+  delete newEmployee.name;
+  
+ return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key, value) {
+  delete employee.name;
+  
+ return employee;
+}
+
+
+
+
+
+console.log(updateEmployeeWithKeyAndValue(employee, person, Sam))
+
